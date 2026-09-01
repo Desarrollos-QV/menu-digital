431d25de31f6b99deeee4cb97a2432b747b669e2
Desarrollos-QV <soporte.desarrollosqv@gmail.com>
2026-09-01 14:05:29 -0600

    fix: inicializacion modular de firebase

--- config/firebase.js
+++ config/firebase.js
@@ -2,12 +2,14 @@ const admin = require('firebase-admin');
 const path = require('path');
 const fs = require('fs');
 
+const { initializeApp, cert } = require('firebase-admin/app');
+
 try {
   const serviceAccountPath = path.join(__dirname, 'firebase-service-account.json');
   if (fs.existsSync(serviceAccountPath)) {
     const serviceAccount = require(serviceAccountPath);
-    admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount)
+    initializeApp({
+      credential: cert(serviceAccount)
     });
     console.log("Firebase Admin initialized successfully");
   } else {
